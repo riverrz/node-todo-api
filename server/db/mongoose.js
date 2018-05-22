55b36ec0305532abc4153c029ed33be57ead959e
shivam123425 <shivam123425@gmail.com>
2018-05-22 19:01:44 +0530

    Refactored server.js and make seperate fles for models

---
+++ server/db/mongoose.js
@@ -0,0 +1,8 @@
+var mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;  // Sets the mongoose promise to use default javascript promises
+mongoose.connect("mongodb://localhost:27017/TodoApp");
+
+module.exports= {
+    mongoose
+}
