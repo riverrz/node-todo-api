80003c0b3659a1a6caca179b3c35586b62fa65f0
shivam123425 <shivam123425@gmail.com>
2018-05-22 19:48:07 +0530

    POST route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
 const {mongoose} = require("./db/mongoose");
 const {Todo} = require("./models/todo");
 const {User} = require("./models/user");
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post("/todos", (req,res)=> {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((createdTodo)=> {
+        res.send(createdTodo);
+    }, (err)=> {
+        res.status(400).send(err);
+    })
+});
+
+app.listen(3000, ()=> {
+    console.log("Server has started");
+});
