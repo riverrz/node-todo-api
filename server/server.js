5b05224654e2f5e77d5dd89229b3fa525cff2f93
shivam123425 <shivam123425@gmail.com>
2018-05-24 15:57:39 +0530

    Add /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { ObjectID } = require("mongodb");
 
 const { mongoose } = require("./db/mongoose");
 const { Todo } = require("./models/todo");
@@ -34,6 +35,21 @@ app.get("/todos", (req, res) => {
     }
   );
 });
+
+app.get("/todos/:id", (req, res) => {
+  if (!ObjectID.isValid(req.params.id)) {
+    return res.send("Invalid Id passed");
+  }
+  Todo.findById(req.params.id).then(todo => {
+    if (!todo) {
+      return res.status(404).send("Cannot find a todo by that id");
+    }
+    res.send(todo);
+  }).catch((e)=> {
+    res.send("Couldn't fetch the todo");
+  })
+});
+
 app.listen(3000, () => {
   console.log("Server has started");
 });
