1ca66d59c276996eeabaa7fc237e6d6c02a28960
shivam123425 <shivam123425@gmail.com>
2018-06-01 22:39:29 +0530

    Add env variable for DATABASEURL in mongoose.connect

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise; // Sets the mongoose promise to use default javascript promises
-mongoose.connect("mongodb://localhost:27017/TodoApp");
+
+mongoose.connect(process.env.DATABASEURL);
 
 module.exports = {
   mongoose
