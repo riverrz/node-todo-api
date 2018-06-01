959a19a9adbc1bfa527dc639ed1f986dba70d93b
shivam123425 <shivam123425@gmail.com>
2018-06-01 20:03:57 +0530

    Add port variable and start scripts for deployment

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ const { User } = require("./models/user");
 
 var app = express();
 
+const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 
 app.post("/todos", (req, res) => {
@@ -52,7 +54,7 @@ app.get("/todos/:id", (req, res) => {
     });
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Server has started");
 });
 
